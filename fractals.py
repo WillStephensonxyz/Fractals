0746651071c570bbe1fac84650a5a1d8f29a11ce
WillStephensonxyz <willstephenson66@gmail.com>
2024-07-11 22:07:13 +1000

    added hotkeys to GUI

--- fractals.py
+++ fractals.py
@@ -1,9 +1,20 @@
 import tkinter as tk 
 from tkinter import ttk 
-import numpy 
-# from fractals import mandelbrot_set, julia_set 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation 
 
 root = tk.Tk()
+
+def fullscreen(event): 
+    state = not root.attributes("-fullscreen") 
+    root.attributes('-fullscreen', state)
+
+def killwindow(event):
+    root.destroy() 
+
+root.bind("<Control-q>", killwindow) 
+root.bind("<Control-f>", fullscreen) 
 
 #Define geometry and disable menu tearoff 
 root.geometry("800x500")
@@ -17,15 +28,14 @@ frame.grid()
 menubar = tk.Menu(root) 
 options = tk.Menu(menubar, tearoff=False) 
 
-options.add_cascade(label="Fullscreen       Ctrl+f") 
-options.add_cascade(label="Generate Pattern Ctrl+n")
-options.add_cascade(label="Exit             Ctrl+k") 
+options.add_cascade(label="Fullscreen       Ctrl+F") 
+options.add_cascade(label="Exit             Ctrl+Q") 
+options.add_cascade(label="Pause            Ctrl+P") 
+options.add_cascade(label="Generate Pattern Ctrl+N")
 
 menubar.add_cascade(label="Options", menu=options) 
 
-#Frame text and button, testing purposes
-ttk.Label(frame, text="Hello World!").grid(column=0, row=0) 
-ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0) 
+# Creating frame widget for animation 
 
 root.config(menu=menubar) 
 root.mainloop() 
